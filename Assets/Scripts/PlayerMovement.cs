6375f9386c6eee3dc64842b1b8baaa4cda23ff73
AZMindroma <animationszentrale@gmail.com>
2023-06-06 11:30:04 +0200

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    public float force = 2000f;
+    public float sideForce = 500f;
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(0, 0, force * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rigid.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rigid.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rigid.position.y < -5f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RollingCube
+Silly
